edecf3382c460a2d168bc47c266f5e29cbad7807
Godspeed1337 <maddust2002@gmail.com>
2021-07-05 11:06:59 +0300

    adding login

--- anon/polls/urls.py
+++ anon/polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as views2
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('polls/', views.polls),
+    path('login/', views2.LoginView.as_view(), name='login'),
 ]
